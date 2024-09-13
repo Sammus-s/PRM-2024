5cab016731b0b738116d89c92c596f1ab4e0cbf0
Sammus-s <samuel.rodrigues2004@hotmail.com>
2024-09-12 21:24:20 -0300

    renake header using Material UI

--- mater-play-frontend/src/app/components/Header/index.tsx
+++ mater-play-frontend/src/app/components/Header/index.tsx
@@ -1,21 +1,32 @@
-import './style.css';
+import { AppBar, Box, Button, Container, Toolbar, Typography, styled } from "@mui/material";
+
+const MenuButton = styled(Button)({
+    color: '#FFF',
+    fontWeight: 400
+})
 
 function Header() {
     return(
-        <header>
-            <div className="container">
-                <div className="toolbar">
-                    <h6>Mater Play</h6>
-                    <nav>
-                        <ul>
-                            <li><a href="#" className='active'>Todos</a></li>
-                            <li><a href="#" className='active'>Séries</a></li>
-                            <li><a href="#" className='active'>Filmes</a></li>
-                        </ul>
-                    </nav>
-                </div>
-            </div>
-        </header>
+        <AppBar>
+            <Container>
+                <Toolbar>
+                    <Typography 
+                      variant="h6"
+                    >
+                        Mater Play
+                    </Typography>
+                    <Box
+                      sx={{
+                        paddingLeft: '1rem'
+                      }}
+                    >
+                        <MenuButton variant="text" disabled>Todos</MenuButton>
+                        <MenuButton variant="text" disabled>Séries</MenuButton>
+                        <MenuButton variant="text" disabled>Filmes</MenuButton>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
     )
 }
 
