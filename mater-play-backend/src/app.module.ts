ae85645d060cbefae279dd92cb76323f166650cc
Sammus-s <samuel.rodrigues2004@hotmail.com>
2024-10-10 22:02:08 -0300

    created use case movies using mvc

--- mater-play-backend/src/app.module.ts
+++ mater-play-backend/src/app.module.ts
@@ -1,26 +1,29 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Movie } from './entities/movie-entity';
+import { MovieController } from './controllers/movie-controller';
+import { MovieService } from './services/movie-service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    TypeOrmModule.forRoot(
-      {
+    TypeOrmModule.forRoot({
         type: 'postgres',
         host: process.env.DB_HOST,
         port: Number(process.env.DB_PORT),
         username: process.env.DB_USER,
         password: process.env.DB_PASS,
         database: process.env.DB_NAME,
-        entities: [],
+        entities: [Movie],
         synchronize: true
-      }
-    )
+      
+    }),
+    TypeOrmModule.forFeature([Movie])
   ],
-  controllers: [],
-  providers: [],
+  controllers: [MovieController],
+  providers: [MovieService]
 })
 export class AppModule {}
